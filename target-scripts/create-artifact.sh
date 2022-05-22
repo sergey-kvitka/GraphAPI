742c8f6394c831bb6357d4046dd71f117d30664f
semura823 <muravevartem@outlook.com>
2022-05-22 14:41:28 +0400

    Add creating artifact

--- target-scripts/create-artifact.sh
+++ target-scripts/create-artifact.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y unzip zip
 
-mvn clean package -
+mvn clean package
 wget https://download.bell-sw.com/java/17.0.3+7/bellsoft-jre17.0.3+7-windows-amd64.zip
 unzip bellsoft-jre17.0.3+7-windows-amd64.zip
 mv jre-17.0.3 jre
